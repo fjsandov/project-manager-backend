f727f5e0cc5da9787083cdbf3162e894922ec9b9
Francisco Sandoval <francisco@mtblc.co>
2019-08-10 16:24:35 -0400

    add missing field to blacklist and minor refactor to controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     build_resource(sign_up_params)
-    resource.save
-    render_resource(resource)
+    if resource.save
+      render json: resource
+    else
+      render json: resource.errors, status: :unprocessable_entity
+    end
   end
 end
