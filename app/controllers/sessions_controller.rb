dedac86175d9d6dae330ff834a15c170e6bf4a05
Francisco Sandoval <francisco@mtblc.co>
2019-08-11 17:16:41 -0400

    adding jwtToken to sessions/create body

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,28 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
-  private
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    render json: {
+      success: true,
+      userId: resource.id,
+      jwtToken: current_token
+    }
+  end
 
   def respond_with(resource, _opts = {})
     render json: resource
   end
 
+  private
+
   def respond_to_on_destroy
     head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
