d8d32156e40be474bd8af15b4a5735aa8bc93eff
Francisco Sandoval <francisco@mtblc.co>
2019-08-12 00:26:27 -0400

    wip - factory bot

---
+++ spec/factory.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email "test@example.org"
+    password "blah123456789"
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'support/factory_bot'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 
@@ -38,7 +40,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -69,6 +71,3 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-
-# Database Cleaner Configuration
-config.use_transactional_fixtures = false
